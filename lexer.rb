ee61d27843f919e0aad1f863d246bd5ee397de8f
Omixxx <xxcortex99@gmail.com>
2024-03-16 13:43:49 +0100

    handled lexer exceptions

--- lexer.rb
+++ lexer.rb
@@ -1,80 +1,94 @@
-require "code-lexer"
-require "json"
-require("csv")
+require 'code-lexer'
+require 'json'
+require('csv')
 
-require_relative("OutputCsv")
+require_relative('OutputCsv')
 
 def convert(method)
-  java_lexer = CodeLexer.get("java")
+  java_lexer = CodeLexer.get('java')
   abstractor = CodeLexer::Abstractor.new.abstract_strings.abstract_numbers.abstract_indentations.abstract_spaces
-  lexed_before = java_lexer.lex(method)
-  stream_before = lexed_before.token_stream(abstractor).to_s
-  return stream_before.gsub("¬", "$").downcase
+  stream_before = ''
+  begin
+    lexed_before = java_lexer.lex(method)
+    stream_before = lexed_before.token_stream(abstractor).to_s
+    stream_before = stream_before.gsub('¬', '$').downcase
+  rescue StandardError
+    raise 'Error in lexer, method too long'
+  end
+
+  stream_before
 end
 
 def process(file_path)
   file_content = File.read(file_path)
   data_hash = JSON.parse(file_content)
-  abstract_method = convert(data_hash["method"])
-  output_csv = OutputCsv.new(
-    data_hash["name"],
-    data_hash["startLine"],
-    data_hash["endLine"],
-    data_hash["classPath"],
-    data_hash["readabilityScore"],
-    data_hash["label"],
-    data_hash["method"],
+  puts("Processing: #{data_hash['name']}")
+  abstract_method = convert(data_hash['method'])
+  OutputCsv.new(
+    data_hash['name'],
+    data_hash['startLine'],
+    data_hash['endLine'],
+    data_hash['classPath'],
+    data_hash['readabilityScore'],
+    data_hash['label'],
+    data_hash['method'],
     abstract_method,
-    "",
-    ""
+    '',
+    ''
   )
-  return output_csv
 end
 
 def explore_path(path)
   output_csv_list = []
 
   if File.directory?(path)
-    entries = Dir.entries(path) - [".", ".."]
+    entries = Dir.entries(path) - ['.', '..']
     entries.each do |entry|
       entry_path = File.join(path, entry)
       if File.directory?(entry_path)
         output_csv_list.concat(explore_path(entry_path))
       else
-        output_csv_list << process(entry_path)
+        begin
+          output_csv_list << process(entry_path)
+        rescue StandardError => e
+          warn("#{e.message}\nSkipping file: #{entry_path}")
+        end
       end
     end
   else
-    output_csv_list << process(path)
+    begin
+      output_csv_list << process(entry_path)
+    rescue StandardError => e
+      puts("Error: #{e.message}\nSkipping file: #{entry_path}")
+    end
   end
 
-  return output_csv_list
+  output_csv_list
 end
 
-if ARGV.empty? or
-    ARGV.length > 1
-  puts("Usage: ruby lexer.rb <project_path>")
+if ARGV.empty? ||
+   ARGV.length > 1
+  puts('Usage: ruby lexer.rb <project_path>')
   exit
 end
 
 argv = ARGV
 
-list = []
 list = explore_path(argv[0].to_s)
 
 csv_struct = []
 csv_struct <<
-  [
-    "name",
-    "startLine",
-    "endLine",
-    "classPath",
-    "readabilityScore",
-    "label",
-    "original_method",
-    "abstract_method",
-    "model_prediction",
-    "manual_flag"
+  %w[
+    name
+    startLine
+    endLine
+    classPath
+    readabilityScore
+    label
+    original_method
+    abstract_method
+    model_prediction
+    manual_flag
   ]
 
 list.each do |output_csv|
@@ -93,7 +107,7 @@ list.each do |output_csv|
     ]
 end
 
-CSV.open("output.csv", "w") do |csv|
+CSV.open('output.csv', 'w') do |csv|
   csv_struct.each do |row|
     csv << row
   end
